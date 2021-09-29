b27fe0746e8a2a803cb2b54d8f18ced8e0ef5d60
unicorns18 <77941625+unicorns18@users.noreply.github.com>
2021-09-29 16:13:58 +1000

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import sys
 
 def main():
     if len(sys.argv) < 2:
-        print("Usage: python3 mkv2mp4.py <mkv_file>")
+        print("Usage: python3 main.py <mkv_file>")
         return
 
     mkv_file = sys.argv[1]
     mp4_file = mkv_file.replace(".mkv", ".mp4")
-
-    cmd = "C:\\Users\\Megan\\Downloads\\ffmpeg-4.3.2-full_build-shared\\ffmpeg-4.3.2-2021-02-27-essentials_build\\bin\\ffmpeg -i " + mkv_file + " -c:v copy -c:a copy " + mp4_file
+    
+    #Replace with your own directory to FFMpeg :)
+    cmd = "ffmpeg -i " + mkv_file + " -c:v copy -c:a copy " + mp4_file
     subprocess.call(cmd, shell=True)
 
 if __name__ == "__main__":
